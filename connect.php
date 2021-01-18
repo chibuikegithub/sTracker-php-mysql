3593d87a29a5d1d5dbd14bc429c0bd676d66c1f4
Chibuike Madubuike <chibuikemadubuike@gmail.com>
2021-01-18 03:53:00 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 	error_reporting(E_ALL ^ E_DEPRECATED);
-	mysql_connect("localhost", "root", "");
+	mysql_connect("hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "uwhahyxl1ws4ct6w", "gxntvc3kc2p2fant ");
 	mysql_select_db("smarttrackerdb");
 
 ?>
